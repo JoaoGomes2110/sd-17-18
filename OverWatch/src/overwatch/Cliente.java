2057f623f4cfafbdda44b9b56112fbe048a8e0f5
Tiago Fraga <tiagofraga@MBP-de-Tiago.home>
2017-12-09 14:36:02 +0000

    Atualizei umas coisas, registo esta a 90%

--- OverWatch/src/overwatch/Cliente.java
+++ OverWatch/src/overwatch/Cliente.java
@@ -8,8 +8,11 @@ package overwatch;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 
 /**
  *
@@ -18,18 +21,106 @@ import java.net.SocketException;
 public class Cliente {
     
     private String hostname;
-    private int porto;
+    private int porta;
     
-    public Socket socket;
-    public BufferedReader in;
-    public BufferedWriter out;
+    private Socket socket;
+    private BufferedReader in;
+    private BufferedWriter out;
     
-    private Jogador jogador;
-    private Heroi heroi;
+    private String username;
+    private String password;
+    
+   
     
     
     public static void main(String[] args) {
-       
+        Cliente c = new Cliente("127.0.0.1",12345);
+        c.clientStart();
+    }
+
+    public Cliente(String string, int i) {
+        this.hostname=string;
+        this.porta=i;
+    }
+
+    public void clientStart() {
+        try {	
+            System.out.println("########################### OverWatch v1.0 ################################");
+            System.out.println("> Connecting to server...");
+            this.socket = new Socket(this.hostname, this.porta);
+            System.out.println("> Connection accepted!");
+			
+            
+            BufferedReader systemIn = new BufferedReader(new InputStreamReader(System.in));
+            this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			
+            System.out.println("********* Choose what do you want ***********");
+            System.out.println("> 1 -> Login");
+            System.out.println("> 2 -> Register");
+            String op = systemIn.readLine();
+            String opResponse = "0";
+            String msgResponse;
+            
+            while(opResponse.equals("0")){
+                System.out.println("> Username: ");
+                this.username = systemIn.readLine();
+            
+                System.out.println("> Password: ");
+                this.password = systemIn.readLine();
+            
+                out.write(op);
+                out.newLine();
+            
+                out.write(this.username);
+                out.newLine();
+            
+                out.write(this.password);
+                out.newLine();
+                
+                out.flush();
+                
+                //Server Response
+                opResponse= in.readLine();
+                msgResponse = in.readLine();
+                System.out.println(msgResponse);
+                    
+            }
+            
+    //*************************************** Login/Register Complete ********************************************************
+    
+    //*************************************** Enter in a  Match *********************************************************
+            System.out.println("********* Choose what do you want ***********");
+            System.out.println("> 1 -> Enter in a Match");
+            System.out.println("> 2 -> Exit the Game");
+            
+            op = systemIn.readLine();
+            out.write(op);
+            out.newLine();
+            out.flush();
+            
+            while(!op.equals("2")){
+                System.out.println("> Loading ...");
+                msgResponse = in.readLine();
+                System.out.println(msgResponse);
+                
+                
+                
+            }
+           
+
+            //fechar sockets
+            systemIn.close();
+            socket.shutdownOutput();
+            socket.shutdownInput();
+            socket.close();
+
+            } catch (UnknownHostException e) {
+                System.out.println("ERRO: Server doesn't exist!");
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
     }
     
     
--- OverWatch/src/overwatch/Server_Worker.java
+++ OverWatch/src/overwatch/Server_Worker.java
@@ -46,52 +46,83 @@ public class Server_Worker implements Runnable {
             username = in.readLine();
             password = in.readLine();
 
-            while(!(sucesso)){
-                if(op.equals("1")){
+            if(op.equals("1")){
+                
+                sucesso = this.server.loginClient(username, password, out);
+                
+                while(!sucesso){
+                    System.out.println("Wrong nickname or password, ask client for other try");
+                    
+                    out.write("0");
+                    out.newLine();
+                        
+                    out.write("Wrong username or password, please try again!");
+                    out.newLine();
+                        
+                    out.flush();
+                        
+                    username = in.readLine();
+                    password = in.readLine();
                     sucesso = this.server.loginClient(username, password, out);
-                    if(!sucesso){
-                        System.out.println("Wrong nickname or password, ask client for other try");
-                        out.write("Wrong username or password, please try again!");
-                        out.newLine();
-                        out.flush();
-                    }
-                    else{
-                        System.out.println("SUCCESS Login : Client entered in the system !!");
-                        out.write("Login Complete!");
-                        out.newLine();
-                        out.flush();
-                    }
                 }
-            
-                else if (op.equals("2")){
-                    sucesso = this.server.registerClient(username, password, out);
-                    if(!sucesso){
-                        System.out.println("This username is already used, ask client for other try");
-                        out.write("This username is already, please choose a diferent one!");
-                        out.newLine();
-                        out.flush();
-                    }
-                    else{
-                        System.out.println("SUCCESS Register : Client entered in the system !!");
-                        out.write("Register Complete!");
-                        out.newLine();
-                        out.flush();
-                    }
-                }
-                else if (sucesso==false){
-                    op= in.readLine();
-                }
+                    
+                System.out.println("SUCCESS Login : Client entered in the system !!");
+                
+                out.write("1");
+                out.newLine();
+                
+                out.write("Login Complete!");
+                out.newLine();
+                        
+                out.flush();
+                    
             }
             
+            if (op.equals("2")){
+                
+                sucesso = this.server.registerClient(username, password, out);
+                
+                while(!sucesso){
+                    System.out.println("This username is already used, ask client for other try");
+                        
+                    out.write("0");
+                    out.newLine();
+                        
+                    out.write("This username is already, please choose a diferent one!");
+                    out.newLine();
+                        
+                    out.flush();
+                        
+                    username = in.readLine();
+                    password = in.readLine();
+                    sucesso = this.server.registerClient(username, password, out);
+                }
+                    
+                System.out.println("SUCCESS Register : Client entered in the system !!");
+                        
+                out.write("1");
+                out.newLine();
+                        
+                out.write("Register Complete!");
+                out.newLine();
+                        
+                out.flush();
+                    
+            }
+                
+               
+                
+            
+           
             Jogador jogador = this.server.getJogador(username);
   //********************************************************************************************************************          
   
   //*********************************************Enter the Game******************************************************
     
             op = in.readLine();
-            sucesso = false;
             String gameName;
-            while(!sucesso){
+            while(op.equals("1")){
+                
                 if(op.equals("1")){
                     boolean game = this.server.hasGame();
                     Jogo actualGame;
@@ -116,31 +147,23 @@ public class Server_Worker implements Runnable {
                         System.out.println("Game "+gameName+" is waiting for the 10 players");
                     }
                     
-                     
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    sucesso = false;
-                    
+                        
                 }else if(op.equals("2")){
-                    sucesso = true;
-                }
                 
+                }
+                op = in.readLine();
             }
        
-  
-  
+            System.out.println("> Client "+ this.username + " exit the system");
+            this.server.shutdownClient(this.username);
+            
+            this.socket.shutdownOutput();
+            this.socket.shutdownInput();
+            this.socket.close();
   
   //*+*******************************************Shutdown Client********************************+++****+++++++++++*++         
         }catch (IOException e) {
 			e.printStackTrace();
-	}
-	finally {
-            this.server.shutdownClient(this.username);			
 	}
         
     }
